c05d599731bbb7c051b71783c4becb9c89f30e73
syh053 <syh053@gmail.com>
2025-08-23 13:49:27 +0800

    build basic page template

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/index")
 def index():
   return render_template("index.html")
+
+# 若 url 都不匹配，自動導向首頁
+@app.route("/")
+def home():
+  return redirect(url_for("index"))
+
 
 if __name__ == "__main__":
   app.run(debug=True)
